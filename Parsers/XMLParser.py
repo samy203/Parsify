e1dc742f353a92b8b7e4e1c1e7de6ff221f7b94a
Scorias <mostafa.samy203@gmail.com>
2021-08-31 23:40:31 +0200

    Adding XML Parser without its parse function

---
+++ Parsers/XMLParser.py
@@ -0,0 +1,11 @@
+from parser import ParserBase
+
+
+class XMLParser(ParserBase):
+
+    def GetFormat(self):
+        return 'xml'
+
+    def Parse(self, path):
+        # implement later
+        pass
