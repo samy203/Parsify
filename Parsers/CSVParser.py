f28fccc9583a0d32815dc22d35261c9a4b8d14fa
Scorias <mostafa.samy203@gmail.com>
2021-09-01 04:39:01 +0200

    Implementing CSV Parser

--- Parsers/CSVParser.py
+++ Parsers/CSVParser.py
@@ -1,5 +1,5 @@
-from ParserBase import  ParserBase
-import xmltodict, json
+from ParserBase import ParserBase
+import json
 import csv
 import os
 
@@ -12,19 +12,38 @@ class CSVParser(ParserBase):
     def Parse(self, paths):
         self.GenerateOutputDirectory()
 
-        customersData ={}
-        with open(paths[0], encoding='utf-8') as cusFile:
-            csvReader = csv.DictReader(cusFile)
-            for rows in csvReader:
-                key = rows['id']
-                customersData[key] = rows
+        outputObj = {'file_name': os.path.basename(paths[0])}
+        # the csv file contained multiple persons compared to xml files, this is why im converting dict['transaction']
+        # from {} to [] , and there is 2 input files, so im changed how the output file is named accordingly
 
-        vehicleData ={}
+        outputObj = {'file_name': f'{os.path.basename(paths[0])}-{os.path.basename(paths[1])}', 'transaction': []}
+
+        vehicleData = []
         with open(paths[1], encoding='utf-8') as vicFile:
             csvReader = csv.DictReader(vicFile)
             for rows in csvReader:
-                key = rows['id']
-                vehicleData[key] = rows
+                vehicleData.append(rows)
 
+        with open(paths[0], encoding='utf-8') as cusFile:
+            csvReader = csv.DictReader(cusFile)
+            for rows in csvReader:
+                transObj = {'date': rows['date']}
+                rows.pop('date')
+                transObj['customer'] = rows
+                transObj['vehicles'] = []
 
+                for vehicle in vehicleData:
+                    if vehicle['owner_id'] == rows['id']:
+                        # cant pop the owner_id , cuz it will get iterated on later, this part could be optmized later by removing cars that already have owner once assigning
+                        # to the transObj['vehicles']
+                        shallowCopyVehicle = vehicle.copy()
+                        shallowCopyVehicle.pop('owner_id')
+                        transObj['vehicles'].append(shallowCopyVehicle)
 
+                outputObj['transaction'].append(transObj)
+
+        with open(
+                f'output/{self.GetFormatExtension()}/{self.GetTimeStamp()}_{os.path.basename(paths[0])}-{os.path.basename(paths[1])}.json',
+                'w',
+                encoding='utf-8') as f:
+            json.dump(outputObj, f, ensure_ascii=False, indent=4)
